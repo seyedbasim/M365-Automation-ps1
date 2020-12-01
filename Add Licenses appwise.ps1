fb99bdd190e80d015f6a4d16a7580297cb2d31fd
Seyed Basim <Seyedbasim95@outlook.com>
2020-12-01 10:49:36 +0530

    Add a new script - licenses appwise

---
+++ Add Licenses appwise.ps1
@@ -0,0 +1,59 @@
+﻿Connect-MsolService
+
+$users = Get-Msoluser -All
+
+foreach($user in $users){
+
+$Licenses = $User.Licenses.AccountSkuId
+
+foreach($License in $Licenses){
+    $Sku = $License.split(":")[1]
+    if($sku -eq "ENTERPRISEPACK"){
+#*****************Add Log*********************************************************************
+        $user.UserPrincipalName >> $logpath
+        $AllLicenses = $User.Licenses | where AccountSkuId -eq $License
+        "License: " + $AllLicenses.AccountSkuId >> $logpath
+        $AllLicenses.ServiceStatus | Format-Table >> $logpath
+        "" >> $logpath
+#*****************Set Apps********************************************************************
+        $LO = New-MsolLicenseOptions -AccountSkuId $License
+        Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -LicenseOptions $LO
+#*********************************************************************************************
+    }elseif($sku -eq "STANDARDPACK"){
+#*****************Add Log*********************************************************************
+        $user.UserPrincipalName >> $logpath
+        $AllLicenses = $User.Licenses | where AccountSkuId -eq $License
+        "License: " + $AllLicenses.AccountSkuId >> $logpath
+        $AllLicenses.ServiceStatus | Format-Table >> $logpath
+        "" >> $logpath
+#*****************Set Apps********************************************************************
+        $LO = New-MsolLicenseOptions -AccountSkuId $License
+        Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -LicenseOptions $LO
+#*********************************************************************************************
+    }elseif($sku -eq "DESKLESSPACK"){
+#*****************Add Log*********************************************************************
+        $user.UserPrincipalName >> $logpath
+        $AllLicenses = $User.Licenses | where AccountSkuId -eq $License
+        "License: " + $AllLicenses.AccountSkuId >> $logpath
+        $AllLicenses.ServiceStatus | Format-Table >> $logpath
+        "" >> $logpath
+#*****************Set Apps********************************************************************
+        $LO = New-MsolLicenseOptions -AccountSkuId $License
+        Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -LicenseOptions $LO
+#*********************************************************************************************
+    }elseif($sku -eq "M365_F1_COMM"){
+#*****************Add Log*********************************************************************
+        $user.UserPrincipalName >> $logpath
+        $AllLicenses = $User.Licenses | where AccountSkuId -eq $License
+        "License: " + $AllLicenses.AccountSkuId >> $logpath
+        $AllLicenses.ServiceStatus | Format-Table >> $logpath
+        "" >> $logpath
+#*****************Set Apps********************************************************************
+        $LO = New-MsolLicenseOptions -AccountSkuId $License
+        Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName -LicenseOptions $LO
+#*********************************************************************************************
+    }
+}
+
+}
+
