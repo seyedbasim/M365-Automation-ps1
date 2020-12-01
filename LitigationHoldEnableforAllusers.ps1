680069815316b07367f0cfbd2e3e45b8c8c1da17
Seyed Basim <Seyedbasim95@outlook.com>
2020-12-01 18:17:16 +0530

    Added new script

---
+++ LitigationHoldEnableforAllusers.ps1
@@ -0,0 +1,11 @@
+﻿#Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File EXOpassword.txt
+
+$password=get-content EXOpassword.txt | ConvertTo-SecureString
+
+$username = "@.onmicrosoft.com"
+
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+
+Connect-ExchangeOnline -Credential $Credential
+
+Get-Mailbox -ResultSize Unlimited -Filter "RecipientTypeDetails -eq 'UserMailbox'" | Where-Object LitigationHoldEnabled -eq $false | Set-Mailbox -LitigationHoldEnabled $true -LitigationHoldDuration 5475
